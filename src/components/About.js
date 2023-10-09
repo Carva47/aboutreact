7b0d4320b5f1730f52d93ee36c3149955a84e5b6
Carva47 <you@example.com>
2023-10-09 04:03:36 +0100

    First Commmit

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About(){
         <h1> About me</h1>
         <div id={about.textabout}>
     <p> Meu Nome é Nvuala Carvalho, estudante de Engenharia Informática, pelo<a href=''>Instituto Superior Politécnico Intercontinental de Luanda,</a>cursando o 5º e ultimo ano. </p>
-        <p>Depios de 3 anos, estou estuando React Js, noavemente e do zero. Este é um portfólio imporovisado onde será feito do que eu for estudando.  Todo conhecimento novo sobre a tecnologia, será implementado aqui como meu teste de conhecimento. Até p momento já pude estudar sobre Instalação de pacotes, sobre os gerenciadores de pactes, como é constituido as app em ReactJS, Componentes e Propriedades bem como conexão com folhas de estilos. </p>
+        <p>Depios de 3 anos, estou estuando React Js, noavemente e do zero. Lembrando que a primeria vez não estudei de forma consistente e não tinha pegado nada como estou pegando agora e foi em apenas duas semanas kkkkk. Este é um portfólio imporovisado onde será feito do que eu for estudando.  Todo conhecimento novo sobre a tecnologia, será implementado aqui como meu teste de conhecimento. Até p momento já pude estudar sobre Instalação de pacotes, sobre os gerenciadores de pactes, como é constituido as app em ReactJS, Componentes e Propriedades bem como conexão com folhas de estilos. </p>
     </div>
         </div>
     
